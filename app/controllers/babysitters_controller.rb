f6d34ed4a0a26bf8c6c83c1e2cfcf0d128a260ff
Bachtiar Eko Wahyudi <bachtiarekowahyudi@gmail.com>
2016-09-19 07:42:37 +0700

    continue update & delete babysitter profile

--- app/controllers/babysitters_controller.rb
+++ app/controllers/babysitters_controller.rb
@@ -6,27 +6,66 @@ class BabysittersController < ApplicationController
 	end
 
 	def create
-			if current_user
-				params[:babysitter][:user_id] = session[:user_id]
-			end
-			skill_ids = params[:babysitter].delete("skill_ids")
-			selected_skills = []
+		if current_user
+			params[:babysitter][:user_id] = session[:user_id]
+		end
+		skill_ids = params[:babysitter].delete("skill_ids")
+		selected_skills = []
 
-			selected_skills = skill_ids.collect{ |skil_id| Skill.find(skil_id)}
+		selected_skills = skill_ids.collect{ |skil_id| Skill.find(skil_id)}
 
-			@babysitter = Babysitter.new(babysitter_params)
-			if @babysitter.save
-				@babysitter.skills = selected_skills
+		@babysitter = Babysitter.new(babysitter_params)
+		if @babysitter.save
+			@babysitter.skills = selected_skills
 
-				redirect_to mains_index_path
-			else
-				render_to 'new'
-			end
+			redirect_to mains_index_path
+		else
+			render_to 'new'
+		end
 	end
 
 	def show
 		@babysitter = Babysitter.find(params[:id])
 		@skills = @babysitter.skills
+	end
+
+	def edit
+	@babysitter = Babysitter.find(params[:id])
+	@skill = Skill.all
+	@babysitter_skills = @babysitter.skills
+	end
+	
+	def update
+		if current_user
+			params[:babysitter][:user_id] = session[:user_id]
+		end
+	@babysitter_skills = Babysitter_skills.find_by_babysitter_id(params[:babysitter][:id])
+	@babysitter_skills.destroy
+	@babysitter = Babysitter.find(params[:id])
+	skill_ids = params[:babysitter].delete("skill_ids")
+	selected_skills = skill_ids.collect{ |skil_id| Skill.find(skil_id)}	
+	@babysitter = @babysitter.update(babysitter_params)
+	if @babysitter.update
+		@babysitter.skills = selected_skills
+		flash[:notice]="Your baby profile has been updated"
+		redirect_to mains_index_path
+	else
+		flash[:aler]="your baby profile failed to update"
+		redirect_to edit_babysitter_path
+	end
+	end
+
+	def destroy
+	@babysitter = Babysitter.find(params[:id])
+	@babysitter_skills = @babysitter.destroy
+
+		if @babysitter_skills
+			flash[:notice] = "babysitter profile has been deleted"
+			redirect_to mains_index_path
+		else
+			flash[:alert] = "babysitter profiled failed to deleted"
+			redirect_to babysitter_path(@babysitter)
+		end
 	end
 
 	private
--- app/models/babysitter.rb
+++ app/models/babysitter.rb
@@ -1,6 +1,6 @@
 class Babysitter < ActiveRecord::Base
-	belongs_to :user
-	has_and_belongs_to_many :skills, join_table: "babysitter_skills"
+	belongs_to :user 
+	has_and_belongs_to_many :skills, join_table: "babysitter_skills", dependent: :destroy
 	mount_uploader :photos, AvatarUploader
 
 	validates :nik , :age, :description, presence: true
